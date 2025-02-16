32113d5bdc4f29fc7e1c9b9bb7d527f1b02c5ec0
polhek <ziga.groselj@gmail.com>
2025-02-16 18:06:29 +0100

    refactor: replace private class fields with public properties in GameClient and Renderer

--- packages/game-client/src/game-client.ts
+++ packages/game-client/src/game-client.ts
@@ -1,25 +1,27 @@
 import { Renderer } from "./renderer";
 
 export class GameClient {
-  private #ctx: CanvasRenderingContext2D;
+  private ctx: CanvasRenderingContext2D;
 
   // Rendering
-  private #renderer: Renderer;
-  private #animationFrameId: number;
+  private renderer: Renderer;
+
+  // State
+  private animationFrameId: number | null = null;
 
   constructor(htmlCanvas: HTMLCanvasElement) {
     console.log("GameClient created");
     console.log("htmlCanvas", htmlCanvas);
-    this.#ctx = htmlCanvas.getContext("2d")!;
+    this.ctx = htmlCanvas.getContext("2d")!;
 
-    this.#renderer = new Renderer(this.#ctx);
+    this.renderer = new Renderer(this.ctx);
   }
 
   public start() {
     const tick = () => {
       this.update();
-      this.#renderer.render();
-      this.#animationFrameId = requestAnimationFrame(tick);
+      this.renderer.render();
+      this.animationFrameId = requestAnimationFrame(tick);
     };
     tick();
   }
--- packages/game-client/src/renderer.ts
+++ packages/game-client/src/renderer.ts
@@ -1,20 +1,20 @@
 export class Renderer {
-  private #ctx: CanvasRenderingContext2D;
+  private ctx: CanvasRenderingContext2D;
 
   constructor(ctx: CanvasRenderingContext2D) {
     console.log("Renderer created");
     console.log("ctx", ctx);
-    this.#ctx = ctx;
+    this.ctx = ctx;
     this.resizeCanvas();
   }
 
   public resizeCanvas() {
-    this.#ctx.canvas.width = window.innerWidth * window.devicePixelRatio;
-    this.#ctx.canvas.height = window.innerHeight * window.devicePixelRatio;
-    this.#ctx.canvas.style.width = `${window.innerWidth}px`;
-    this.#ctx.canvas.style.height = `${window.innerHeight}px`;
-    this.#ctx.imageSmoothingEnabled = false;
-    this.#ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    this.ctx.canvas.width = window.innerWidth * window.devicePixelRatio;
+    this.ctx.canvas.height = window.innerHeight * window.devicePixelRatio;
+    this.ctx.canvas.style.width = `${window.innerWidth}px`;
+    this.ctx.canvas.style.height = `${window.innerHeight}px`;
+    this.ctx.imageSmoothingEnabled = false;
+    this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
   }
 
   public render() {
